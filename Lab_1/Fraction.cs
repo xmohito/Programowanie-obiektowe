1278d77355c2e2841426f8420bacb8f0b10a9339
Michał Brózda <102979176+xmohito@users.noreply.github.com>
2022-04-22 18:01:48 +0200

    Add files via upload

---
+++ Lab_1/Fraction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    public class Fraction
+    {
+        private double numerator { get; set; }      //licznik
+        private double denominator { get; set; }     //mianownik
+
+        //konstruktor
+        public Fraction()
+        {
+            numerator = 1;
+            denominator = 2;
+        }
+
+        //konstruktor dwu argumentowy
+        public Fraction(double numerator, double denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        //konstruktor kopiujący
+        public Fraction(Fraction previousFraction)
+        {
+            numerator = previousFraction.numerator;
+            denominator = previousFraction.denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"Numerator: {numerator}, denominator: {denominator}";
+        }
+
+        public static Fraction operator +(Fraction x1) => x1;
+        public static Fraction operator -(Fraction x1) => new Fraction(-x1.numerator, x1.denominator);
+        public static Fraction operator +(Fraction x1, Fraction x2) => new Fraction(x1.numerator * x2.denominator + x2.numerator * x1.denominator, x1.denominator * x2.denominator);
+        public static Fraction operator -(Fraction x1, Fraction x2) => new Fraction(x1.numerator * x2.denominator - x2.numerator * x1.denominator, x1.denominator * x2.denominator);
+        public static Fraction operator *(Fraction x1, Fraction x2) => new Fraction(x1.numerator * x2.numerator, x1.denominator * x2.denominator);
+        public static Fraction operator /(Fraction x1, Fraction x2) => new Fraction(x1.numerator * x2.denominator, x2.denominator * x1.numerator);
+
+    }
+}
---
+++ Lab_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction x1 = new Fraction();
+            Fraction x2 = new Fraction(1, 4);
+            Console.WriteLine(x1); Console.WriteLine(x2); Console.WriteLine();
+            Console.WriteLine(-x1);
+            Console.WriteLine(x1+x2);
+
+        }
+        string Konstruktory()
+        {
+            //Żeby działało trzeba przekopiować do main
+            Fraction x2 = new Fraction();
+            Console.WriteLine(x2);
+
+            Fraction f2 = new Fraction(5, 7);
+            Console.WriteLine(f2);
+
+            Fraction x3 = new Fraction(f2);
+            Console.WriteLine(x3);
+            return "";
+        }
+    }
+}
