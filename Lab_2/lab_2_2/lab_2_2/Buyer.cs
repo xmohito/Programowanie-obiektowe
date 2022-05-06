6bd4db3d618324a6f49ac9f237315455584c4671
Michał Brózda <102979176+xmohito@users.noreply.github.com>
2022-05-07 00:51:23 +0200

    Lab_2
    Lab_2

---
+++ Lab_2/lab_2_2/lab_2_2/Buyer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_2
+{
+    class Buyer : Person
+    {
+        protected List<Product> products = new List<Product>();
+        public Buyer(string name, int age) : base(name, age)
+        {
+
+        }
+
+        public void AddProduct(Product product)
+        {
+            this.products.Add(product);
+        }
+
+        public void RemoveProduct(int index)
+        {
+            this.products.RemoveAt(index);
+        }
+
+
+        public override void Print(string prefix)
+        {
+            Console.WriteLine(prefix + "Buyer: " + this.name + " (" + this.age.ToString() + " y.o.)");
+            if (products.Count > 0)
+            {
+                Console.WriteLine(prefix + "\t -- Products: --");
+                foreach (var p in products)
+                {
+                    p.Print(prefix + "\t");
+                }
+            }
+        }
+    }
+}
---
+++ Lab_2/lab_2_2/lab_2_2/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_2
+{
+    abstract class Person : IThing
+    {
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        protected string name;
+        protected int age;
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Age
+        {
+            get { return age; }
+        }
+
+        public bool Equals(Person obj)
+        {
+            return obj.name == this.name && obj.age == this.age;
+        }
+
+        public abstract void Print(string prefix);
+
+    }
+}
