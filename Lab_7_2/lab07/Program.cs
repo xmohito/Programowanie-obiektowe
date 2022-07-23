9f406c4842d01bbdae1e6b475f469685f91d1a99
Michał Brózda <102979176+xmohito@users.noreply.github.com>
2022-07-23 12:10:15 +0200

    Add files via upload

---
+++ Lab_7_2/lab07/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hint: change `DESKTOP-123ABC\SQLEXPRESS` to your server name
+            //       alternatively use `localhost` or `localhost\SQLEXPRESS`
+            Task task = DoTasks();
+
+            task.Wait();
+            string connectionString = @"Data Source=DESKTOP-RFJRJ41\SQLEXPRESS;Initial Catalog=blogdb;Integrated Security=True";
+
+            using (BloggingContext db = new BloggingContext(connectionString))
+            {
+                var usss = new User
+                {
+                    FirstName = "Andrzej",
+                    LastName = "Jawlak",
+                    Age = 11,
+                    City = "krakow",
+                    Etnicity = "dddd",
+                    Gender = "nierozpoznano",
+                };
+                db.User.Add(usss);
+                db.SaveChanges();
+                Console.WriteLine("Querying for a user");
+
+                var user = db.User.Where(b => b.Id == usss.Id);
+                Console.WriteLine(user.Count());
+                Console.WriteLine("Delete the user");
+
+                db.Remove(user);
+                db.SaveChanges();
+            }
+
+        }
+        private static async Task DoTasks()
+        {
+            int couter = 0;
+
+            for (int i = 0; i < 12; i += 3)
+            {
+              
+                Task<bool> task1 = DoTask(i + 0);
+                Task<bool> task2 = DoTask2(i + 1);
+              
+
+                // waiting for the all results
+                if (await task1) couter += 1;
+                if (await task2) couter += 1;
+                
+            }
+
+            Console.WriteLine("Tasks done: " + couter);
+        }
+
+        private static async Task<bool> DoTask(int taskId)
+        {
+            return await Task<bool>.Run(() =>
+            {
+                string connectionString = @"Data Source=DESKTOP-RFJRJ41\SQLEXPRESS;Initial Catalog=blogdb;Integrated Security=True";
+
+                using (BloggingContext db = new BloggingContext(connectionString))
+                {
+
+                    Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+                    // Create
+                    Console.WriteLine("Inserting a new user");
+
+                    db.Add(new User
+                    {
+                        FirstName = "Andrzej",
+                        LastName = "Jawlak",
+                        Age = 11,
+                        City = "krakow",
+                        Etnicity = "dddd",
+                        Gender = "nierozpoznano",
+                    });
+                    db.SaveChanges();
+
+                }
+
+                return true;
+            });
+        }
+        private static async Task<bool> DoTask2(int taskId)
+        {
+            return await Task<bool>.Run(() =>
+            {
+                string connectionString = @"Data Source=DESKTOP-RFJRJ41\SQLEXPRESS;Initial Catalog=blogdb;Integrated Security=True";
+
+                using (BloggingContext db = new BloggingContext(connectionString))
+                {
+                    db.Add(new User
+                    {
+                        FirstName = "Testerawaita",
+                        LastName = "tester",
+                        Age = 33,
+                        City = "krakow",
+                        Etnicity = "aaa",
+                        Gender = "helikopter",
+                    });
+                    db.SaveChanges();
+
+                 
+
+                }
+
+                return true;
+            });
+        }
+    }
+}
---
+++ Lab_7_2/lab07/lab07.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<User> User { get; set; }
+    public DbSet<Roles> roles { get; set; }
+    public DbSet<Tasks> tasks { get; set; }
+
+    public string ConnectionString { get; }
+
+    public BloggingContext(string connectionString)
+    {
+        this.ConnectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(this.ConnectionString);
+    }
+}
+
+public class Blog
+{
+    public long Id { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+   
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+    public string Etnicity { get; set; }
+    public string Gender { get; set; }
+    public List<Roles> Roles { get; } = new();
+}
+
+public class Roles
+{
+    public int Id { get; set; }
+    public string JobName { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public List<Tasks> Tasks { get; } = new();
+}
+
+public class Tasks
+{
+    public int Id { get; set; }
+    public string TaskName { get; set; }
+    public int RolesId { get; set; }
+    public Roles Roles { get; set; }
+}
